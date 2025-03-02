28041bd1cffa557dcfb3c0f0d62a2867b56584b0
taranramu <taranramu@gmail.com>
2025-03-02 10:09:39 -0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,6 @@ import path from "path";
 import cors from "cors";
 
 import { connectDB } from "./config/db.js";
-
 import productRoutes from "./routes/product.route.js";
 
 dotenv.config();
@@ -12,27 +11,28 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-const __dirname = path.resolve();
+// Change dirname to point to root directory
+const __dirname = path.resolve(path.join(process.cwd(), '..'));
 
-app.use(express.json()); // allows us to accept JSON data in the req.body
+app.use(express.json());
 
 app.use(cors({
-	origin: "http://localhost:5173",
-	methods: ["GET", "POST", "PUT", "DELETE"],
-	allowedHeaders: ["Content-Type"]
-  }));
-  
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type"]
+}));
 
 app.use("/api/products", productRoutes);
 
 if (process.env.NODE_ENV === "production") {
-	app.use(express.static(path.join(__dirname, "/client/build/server")));
-	app.get("*", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "client", "build", "server", "index.js"));
-	});
+    // Updated paths to start from root directory
+    app.use(express.static(path.join(__dirname, "client/build/server")));
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "client", "build", "server", "index.js"));
+    });
 }
 
 app.listen(PORT, () => {
-	connectDB();
-	console.log("Server started at http://localhost:" + PORT);
+    connectDB();
+    console.log("Server started at http://localhost:" + PORT);
 });
