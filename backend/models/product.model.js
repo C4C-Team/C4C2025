2707b7c037710b4d0508d6027c9a160a417c286a
Josue Vicente <josue.vicente@ufl.edu>
2025-03-17 05:15:55 -0400

    Testing False Location Requirement
    Testing redeployment of schema change

--- backend/models/product.model.js
+++ backend/models/product.model.js
@@ -6,7 +6,7 @@ const productSchema = new mongoose.Schema({
             lat: Number,
             lng: Number,
         },
-        required: true,
+        required: false,
     },
     severity: {
         type: String,
